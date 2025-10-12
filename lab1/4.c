a32db31554318dd758794446b4f392a4dc2bbf48
Pochitam <142022315+Pochitam@users.noreply.github.com>
2025-10-12 23:55:20 +0300

    4.c

--- lab1/4.c
+++ lab1/4.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func(int* ptrArr, int n){
-    for(int i=0; i<n; i++){
-        *(ptrArr+n+i) = *(ptrArr+i);
+void func(int* ptrArr, int n1, int* ptrArr2, int n2){
+    for(int i=0; i<n1; i++){
+        *(ptrArr2+n2+i) = *(ptrArr+i);
     }
+    return;
+}
+
+void vvod(int* ptrArr, int n1){
+    for (int i=0; i<n1; ++i){
+        scanf("%d", ptrArr+i);
+    }
+    return;
 }
 
 int main(){
-    int n;
-    scanf("%d", &n);
-    int* ptrArr = (int*)malloc(2*n*sizeof(int));
-    int* ptrArr2 = ptrArr+n;
-    for(int i=0; i<n; ++i){
-        scanf("%d", ptrArr+i);
-    }
-    func(ptrArr, n);
-    for (int i=0; i<n; ++i){
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+    int* ptrArr = (int*)malloc(n1*sizeof(int));
+    int* ptrArr2 = (int*)malloc(n2*sizeof(int)+n1*sizeof(int));
+    vvod(ptrArr, n1);
+    vvod(ptrArr2, n2);
+    func(ptrArr, n1, ptrArr2, n2);
+    for (int i=0; i<n2+n1; ++i){
         printf("%d ", *(ptrArr2+i));
     }
     free(ptrArr);
+    free(ptrArr2);
 }
