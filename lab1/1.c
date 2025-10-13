90af1d499cd031081b85c55d97961e4015245d23
Pochitam <142022315+Pochitam@users.noreply.github.com>
2025-10-13 16:47:43 +0300

    1.c

--- lab1/1.c
+++ lab1/1.c
@@ -1,10 +1,10 @@
-// среднне значение int или double
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(){
     int n;
+    printf("Введите размер массива:\n");
     scanf("%d", &n);
     int* ptrArr = (int*)malloc(n*sizeof(int));
     srand(time(NULL));
@@ -13,14 +13,15 @@ int main(){
         *x = rand()%16256;
     }
     int min=16256, max=0, sr=0; 
+    printf("Элементы массива:\n");
     for (int i=0; i<n; ++i){
         int el = *(ptrArr+i);
-        min = (el < min ? el: min);
-        max = (el>max ? el : max);
+        min = el < min ? el: min;
+        max = el>max ? el : max;
         sr += el;
-        printf("%d\n", *(ptrArr+i));
+        printf("%d ", *(ptrArr+i));
     }
     free(ptrArr);
-    printf("Max %d; Min %d; Average %d\n", max, min, sr/n);
+    printf("\nMax %d; Min %d; Average %d\n", max, min, sr/n);
     return 0;
 }
