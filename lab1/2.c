529203bbec14093466a2acd05fb05f3b9a48bd33
Pochitam <142022315+Pochitam@users.noreply.github.com>
2025-10-13 16:48:11 +0300

    2.c

--- lab1/2.c
+++ lab1/2.c
@@ -1,4 +1,3 @@
-// на вход даются целые числа или дробные? вывод евклидова типа флоат?
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -17,7 +16,8 @@ int calculateArea(double x1, double y1, double x2, double y2){
 
 int main(){
     Point p1; Point p2;
+    printf("Координаты двух точек через пробел:\n");
     scanf("%lf %lf %lf %lf", &p1.x, &p1.y, &p2.x, &p2.y);
-    printf("%lf\n", calculateDistance(p1.x, p1.y, p2.x, p2.y)); 
-    printf("%d\n", calculateArea(p1.x, p1.y, p2.x, p2.y));
+    printf("Евклидово расстояние: %lf\n", calculateDistance(p1.x, p1.y, p2.x, p2.y)); 
+    printf("Площадь: %d\n", calculateArea(p1.x, p1.y, p2.x, p2.y));
 }
